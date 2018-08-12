8b5c66f80d4fa0b7f9f2e9eb796c49f025e05089
dazimmermann <dz10@deanzimmermann.com>
2018-08-12 11:56:46 -0700

    Add project files.

---
+++ PurpleAir/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurpleAir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
